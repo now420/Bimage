1c676b86bebd7a450b2c2984c0ad5726b58b2973
now420 <167709919+now420@users.noreply.github.com>
2024-04-21 19:36:38 -0400

    Update decoder.py

--- versions/py/decoder.py
+++ versions/py/decoder.py
@@ -1,24 +1,25 @@
+# Version 2 (Python) of Bimage by @now420 on github, saqqwd on discord.
 from PIL import Image
 
-def decode(image_file):
-    image = Image.open(image_file)
+def decode(input_file="bw_image.png"):
+    image = Image.open(input_file)
+    pixels = image.load()
 
     binary_data = ""
-    pixels = image.load()
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
-            binary_data += "1" if pixels[i, j] > 127 else "0"
+    width, height = image.size
+
+    for i in range(width):
+        for j in range(height):
+            pixel_value = pixels[i, j]
+            binary_data += '1' if pixel_value == 0 else '0'
 
     text = ""
     for i in range(0, len(binary_data), 8):
-        # Convert each 8-bit binary chunk to a character
-        char = chr(int(binary_data[i:i+8], 2))
-        text += char
-
-    text = text.replace("ÿ", "")
+        byte = binary_data[i:i+8]
+        text += chr(int(byte, 2))
 
     return text
-    
+
 # Usage
 decoded_text = decode("bw_image.png")
-print("Decoded Text:", decoded_text)
+print("Decoded text:", decoded_text)
