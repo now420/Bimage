e53e3e2b3043a5be170e840a12fbe104c3fd567c
now420 <167709919+now420@users.noreply.github.com>
2024-04-21 19:21:42 -0400

    Create decoder.py

---
+++ versions/py/decoder.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+def decode(image_file):
+    image = Image.open(image_file)
+
+    binary_data = ""
+    pixels = image.load()
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            binary_data += "1" if pixels[i, j] > 127 else "0"
+
+    text = ""
+    for i in range(0, len(binary_data), 8):
+        # Convert each 8-bit binary chunk to a character
+        char = chr(int(binary_data[i:i+8], 2))
+        text += char
+
+    text = text.replace("ÿ", "")
+
+    return text
+
+decoded_text = decode("bw_image.png")
+print("Decoded Text:", decoded_text)
